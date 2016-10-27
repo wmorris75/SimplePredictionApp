4430edd091b04436efed4fb719dd5f9c3a32971e
wmorris75 <waynemorris75@gmail.com>
2016-10-26 23:37:49 -0400

    Updated app for making actual prediction based on input data

--- server.R
+++ server.R
@@ -14,35 +14,87 @@ data("mtcars")
 mpgData <- mtcars
 mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
 
-get_pred<-function(data, algorithm, predictor){
-  pred_variable <- gsub("\\s", ".", predictor)
-  inTrain <- createDataPartition(data[[pred_variable]], p=0.7, list=FALSE)
-  training<-data[inTrain,]
-  testing<-data[-inTrain,]
-  
-  #set.seed(323)
-  nbFit<-train(training[[pred_variable]] ~ ., data=training, method=algorithm)
-  nbprediction<-predict(nbFit, testing) 
-  output<-confusionMatrix(nbprediction, testing[[pred_variable]])$overall[1]
-  return (output)
+
+build_formula<-function(value_to_predict, input_vector){
+  vector<-get_feature_entry(input_vector)
+  formula<-paste(value_to_predict, " ~ ")
+  if(length(vector) > 1){
+    for(item in 2:length(vector) - 1){print(item);
+      print(formula)
+      formula<-paste(formula, vector[item], " + ")
+    }
+  }
+  formula <- paste(formula, vector[length(vector)])
+  print(formula)
+  return(formula)
+}
+
+get_df_names<-function(df){
+  names <- names(df)
+  name_values <- strsplit(names, " ")
+  vector <- c()
+  for (i in 1:length(name_values)){
+    vector <- c(vector, name_values[i])
+  }
+  return (vector)
+}
+
+get_feature_entry<-function(input){
+  sep_val <- strsplit(input, ",")
+  name_values <- strsplit(sep_val[[1]], " ")
+
+  vector <- c()
+  for (i in 1:length(name_values)){
+    vector <- c(vector, name_values[i])
+  }
+  return (vector)
+}
+
+train_data_partitiion<-function(data, algorithm, predictor){
+    pred_variable <- gsub("\\s", ".", predictor)
+    inTrain <- createDataPartition(data[[pred_variable]], p=0.7, list=FALSE)
+    return(inTrain)
+}
+
+overall_accurracy<-function(predicted, actual){
+  diff = actual - predicted
+  diff[which(diff < 0)] = -1 * diff[which(diff < 0)]
+  accuracy <- 100 * (1 - mean((diff/actual)/actual))
+  return(accuracy)
+}
+
+determine_data_type<-function(input){
+    for(value in 1:length(input)){
+      # if(length(grep("^[[:digit:]]$ | ^.[[:digit:]] | ^[[:digit:]].[[:digit:]]$", input[value])) > 0){
+      if(!(is.na(as.numeric(input[value])))){
+        input[value] <- as.numeric(input[value])
+      } 
+    }
+  return(input)
 }
 
 #Enter the attribute as it appears in the csv 
-get_prediction<-function(data, algorithm, predictor){
+get_prediction<-function(data, algorithm, predictor, inTrain, option, features){
   out <- tryCatch({
     pred_variable <- gsub("\\s", ".", predictor)
-    inTrain <- createDataPartition(data[[pred_variable]], p=0.7, list=FALSE)
+    # inTrain <- createDataPartition(data[[pred_variable]], p=0.7, list=FALSE)
     training<-data[inTrain,]
     testing<-data[-inTrain,]
+
+    if(option == "sel_feature"){
+      formula = build_formula(pred_variable, features)
+      nbFit<-train(as.formula(formula), data=training, method=algorithm)
+    }
+    else{
+      nbFit<-train(training[[pred_variable]] ~ ., data=training, method=algorithm)
+    }
     
-    #set.seed(323)
-    nbFit<-train(training[[pred_variable]] ~ ., data=training, method=algorithm)
-    nbprediction<-predict(nbFit, testing)
-    
+    return (nbFit)
+    nbprediction<-predict(nbFit, testing) 
     #Accuracy
     e_message<-""
     results = tryCatch({
-      output<-confusionMatrix(nbprediction, testing[[pred_variable]])$overall[1]
+      output<-table(nbprediction, testing[[pred_variable]])#$overall[1]
       return(output)
     },
     error = function(e){
@@ -52,7 +104,7 @@ get_prediction<-function(data, algorithm, predictor){
       
     }) #end of inner tryCatch()
     if(grep("most likely no values", results, ignore.case=TRUE)){
-      output<-table(nbprediction, testing[[pred_variable]])$overall[1]
+      output<-table(nbprediction, testing[[pred_variable]])#$overall[1]
       return(output)
     }
   }, error = function(e){
@@ -68,9 +120,9 @@ shinyServer(
   function(input, output) {
     output$file <- renderText(paste("Filename: ", {input$filename}))
     output$pred <- renderText(paste("Value to forecast: ", {input$predictor}))
-     
-    
+
     output$value<-renderText({
+
       input$predict
       if({input$predictor != ""}){
 
@@ -82,16 +134,48 @@ shinyServer(
         df<-read.csv(inFile$datapath, header=input$header, sep=input$sep, 
                  quote=input$quote)
         
-        out<-{get_prediction(df, input$method, input$predictor)}
-        if(is.numeric(out)){
-          isolate(paste("Accuracy is: ", out))
+        inTrain <- train_data_partitiion(df, input$method, input$predictor)
+        train_data <- df[inTrain,]
+        test_data <- df[-inTrain,]
+        names <- get_df_names(train_data)
+
+        if(input$all_features == "sel_feature"){
+          vector<- get_feature_entry(input$features)
+          print (vector)
+          testing <- subset(test_data, select=as.character(vector))
+          names <- head(testing)
         }
-        else if(is.character(out)){
-          isolate(paste("ERROR ----> ", out))
+        
+        feature_value<-get_feature_entry(input$feature_value)
+        
+        if (length(feature_value)> 0){
+          feature_value<-determine_data_type(feature_value)
         }
-        else if(is.table(out)){
-          isolate(paste("TABLE ----> ", out))
-        }
+        n_columns <- length(feature_value)
+
+        df_forecast <- data.frame(matrix(ncol = n_columns, nrow = 0))
+        colnames(df_forecast) <- get_feature_entry(input$features)
+
+        f_value<-rbind(df_forecast, feature_value)
+        names(f_value) <- names(testing)
+        option <- input$all_features
+        features <- input$features
+        
+        model<-get_prediction(df, input$method, input$predictor, inTrain, option, features)
+        
+        prediction<-predict(model, testing)
+        out<-predict(model, f_value)
+        isolate(paste("Result is: ", out))
+        # out<-{get_prediction(df, input$method, input$predictor, inTrain, option, features)}
+        # if(is.numeric(out)){
+        #   isolate(paste("Result is: ", out))
+        # }
+        # else if(is.character(out)){
+        #   isolate(paste("ERROR ----> ", out))
+        # }
+        # else if(is.table(out)){
+        #   isolate(paste("TABLE ----> ", out))
+        # }
       }
     })
     
--- ui.R
+++ ui.R
@@ -1,35 +1,50 @@
 library(shiny)
 shinyUI(pageWithSidebar(
-  headerPanel("Lets Make a Prediction APP"),
+  headerPanel("General Purpose Prediction App"),
   sidebarPanel(
+    
+
+    checkboxInput('header', 'Header', TRUE),
+    radioButtons('sep', 'Separator',
+                 c(Comma=',',
+                   Semicolon=';',
+                   Tab='\t'),
+                 ',', inline=T),
+    tags$hr(),
+    radioButtons('pred_option', 'What are you trying to predict?',
+                 c('Numeric Value'='numeric',
+                   'Boolean Value'="bool"),
+                 inline=T ),
+    
+    tags$hr(),
+    radioButtons('quote', 'Quote',
+                 c(None='',
+                   'Double Quote'='"',
+                   'Single Quote'="'"),
+                 '"', inline=T),
+
+    tags$hr(),
+    #textInput(inputId="filename", label = "Input Filename(Path)"),
+
+    textInput(inputId="predictor", label = "State the parameter you want to predict as it appears in file"),
+    
+    radioButtons("all_features", "Features:", c("Use selected features" = "sel_feature", "Use all features" = "use_all_features")),
+    textInput(inputId="features", label = "List features/parameters to be used for prediction (features should be comma separated)"),
+    textInput(inputId="feature_value", label = "Enter each feature value with comma separation"),
+    
+    radioButtons("method", "Methods:",
+                 c("Random Forest" = "rf",
+                   "Bagging" = "gbm",
+                   "Rpart" = "rpart",
+                   "General Linear Model" = "glm"),
+                 inline=T),
+    tags$hr(),
     
     fileInput('file1', 'Choose CSV File',
               accept=c('text/csv', 
                        'text/comma-separated-values,text/plain', 
                        '.csv')),
     tags$hr(),
-    checkboxInput('header', 'Header', TRUE),
-    radioButtons('sep', 'Separator',
-                 c(Comma=',',
-                   Semicolon=';',
-                   Tab='\t'),
-                 ','),
-    radioButtons('quote', 'Quote',
-                 c(None='',
-                   'Double Quote'='"',
-                   'Single Quote'="'"),
-                 '"'),
-
-    
-    #textInput(inputId="filename", label = "Input Filename(Path)"),
-
-    textInput(inputId="predictor", label = "State value to be predicted as it appears in file"),
-    radioButtons("method", "Methods:",
-                 c("Random Forest" = "rf",
-                   "Bagging" = "gbm",
-                   "Rpart" = "rpart",
-                   "General Linear Model" = "glm")
-    ),
     actionButton("predict", "Predict")
   ),
   mainPanel(
